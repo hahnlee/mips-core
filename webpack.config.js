d73d430f2d619b5b7eff4fe6b16f5f6a05c82cc8
sn0wle0pard <hanlee.dev@gmail.com>
2018-04-26 00:26:33 +0900

    add declarations setting

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
         use: [
           {
             loader: 'awesome-typescript-loader',
+            options: {
+              useBabel: true,
+            },
           },
         ],
       },
